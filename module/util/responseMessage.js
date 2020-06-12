f027b42cbfc3e0e52129480ce794ab6fa66ec21c
ooeunz <yuns994@gmail.com>
2020-06-12 20:08:45 +0900

    add util file

---
+++ module/util/responseMessage.js
@@ -0,0 +1,25 @@
+export default {
+  // BASIC
+  NULL_VALUE: '필요한 값이 없습니다.',
+  OUT_OF_VALUE: '파라미터 값이 잘못 되었습니다.',
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+  // USER
+  SIGN_UP_SUCCESS: '회원가입 성공',
+  SIGN_UP_FAIL: '회원 가입 실패',
+  SIGN_IN_SUCCESS: '로그인 성공',
+  SIGN_IN_FAIL: '로그인 실패',
+  ALREADY_ID: '존재하는 ID 입니다.',
+  NO_USER: '존재하지 않는 유저 id 입니다.',
+  MISS_MATCH_PW: '비밀번호가 일치하지 않습니다',
+
+  // LOG
+  CLICK_LOG_SUCCESS: '클릭로그 성공',
+  CLICK_LOG_FAIL: '클릭로그 실패',
+
+  LIKE_SUCCESS: '찜 목록 읽기 || 수정 성공',
+  LIKE_FAIL: '찜 목록 읽기 || 수정 실패',
+
+  LIKE_VER_GRID_VIEW_SUCCESS: '찜목록 그리드뷰 전달버전 읽기 성공',
+  LIKE_VER_GRID_VIEW_FAIL: '찜목록 그리드뷰 전달버전 읽기 실패',
+};
